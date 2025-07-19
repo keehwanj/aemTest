4620eb6547032eb33ebd0fbe6f3e9f743ade1444
Keehwan Jee <keehwan.jee@snb.ca>
2025-07-19 17:26:50 -0300

    second commit

---
+++ ui.frontend/src/main/webpack/site/main.ts
@@ -0,0 +1,14 @@
+
+// Stylesheets
+import "./main.scss";
+
+// Javascript or Typescript
+import "./**/*.js";
+import "./**/*.ts";
+import '../components/**/*.js';
+import dayjs from "dayjs";
+
+
+const x = dayjs().format();
+
+console.log(x);
